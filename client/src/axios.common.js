d9bb8121a57cedf359870180f074fd3002385791
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-11 22:34:25 +0300

    fix url

--- client/src/axios.common.js
+++ client/src/axios.common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost/api',
+  baseURL: 'https://still-basin-52234.herokuapp.com/api',
   headers: {
     'Content-type': 'application/json',
   },
