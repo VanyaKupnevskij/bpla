4927f8a3dd5682b4a7c2f4ea622f651f1b2cf215
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-11 13:00:29 +0300

    for deploy

--- client/src/axios.common.js
+++ client/src/axios.common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://localhost/api',
   headers: {
     'Content-type': 'application/json',
   },
